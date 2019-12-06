53ce786433a27e1db468b17fc48130734097b3d2
Your Name <you@example.com>
2019-12-06 09:40:31 +0200

    server file added for deploy

---
+++ reflix/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const api = require('./server/routes/api')
+const port = process.env.PORT || 1309
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
